fab8686f2193de24e53f576faa5c0e2073a466a1
Vitor <frete497@gmail.com>
2022-03-27 14:07:47 -0400

    depoimento slider

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+/* Menu-mobile */
 const btnMobile = document.querySelector(".btn-mobile");
 
 function toggleMenu() {
@@ -6,3 +7,28 @@ function toggleMenu() {
 }
 
 btnMobile.addEventListener("click", toggleMenu);
+
+/* slide-depoimento */
+var depoimentos = document.querySelectorAll(".depoimento");
+var btns = document.querySelectorAll(".btn-slider");
+let currentSlide = 1;
+
+var manualNav = function (manual) {
+  depoimentos.forEach((slide) => {
+    slide.classList.remove("ativo");
+
+    btns.forEach((btn) => {
+      btn.classList.remove("ativo");
+    });
+  });
+
+  depoimentos[manual].classList.add("ativo");
+  btns[manual].classList.add("ativo");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
+  });
+});
